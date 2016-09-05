cc0d733cc37e8ed41435cde631342516384b450d
Manoj Welandagoda <manoj.welandagoda@auxenta.com>
2016-09-05 22:51:46 +0530

    update

---
+++ REPORT/GraphReports/src/java/com/biz/report/controller/CommonURLController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biz.report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ *
+ * @author Aux-058
+ */
+@Controller
+public class CommonURLController {
+    
+    @RequestMapping(value = "item-report", method = RequestMethod.GET)
+    public String loadItemReport() {
+        return "item_report";
+    }
+}
