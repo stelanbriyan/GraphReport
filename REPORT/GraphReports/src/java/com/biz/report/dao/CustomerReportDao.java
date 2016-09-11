8abe2f9f9f435351c8fa2ff5a96c3dd98c248dbb
Manoj Welandagoda <manoj.welandagoda@auxenta.com>
2016-09-11 19:38:44 +0530

    update

---
+++ REPORT/GraphReports/src/java/com/biz/report/dao/CustomerReportDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biz.report.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author Aux-058
+ */
+public interface CustomerReportDao {
+    
+    List readCustomers();
+    
+    List read(String customers, String months, String year);
+    
+    List read(String customers, String year);
+}
