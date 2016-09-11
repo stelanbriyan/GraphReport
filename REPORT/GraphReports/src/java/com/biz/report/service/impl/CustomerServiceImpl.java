90d8549a2a5892624b95236cceb999bd2f878eb9
Manoj Welandagoda <manoj.welandagoda@auxenta.com>
2016-09-11 19:45:59 +0530

    update

--- REPORT/GraphReports/src/java/com/biz/report/service/impl/CustomerServiceImpl.java
+++ REPORT/GraphReports/src/java/com/biz/report/service/impl/CustomerServiceImpl.java
@@ -8,6 +8,7 @@ package com.biz.report.service.impl;
 import com.biz.report.dao.CustomerReportDao;
 import com.biz.report.domain.MappingEngine;
 import com.biz.report.domain.Report1;
+import com.biz.report.domain.Report2;
 import com.biz.report.dto.DataPoint;
 import com.biz.report.dto.Report1DataSet;
 import com.biz.report.dto.Report2DataSet;
@@ -73,23 +74,106 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     public List<Report2DataSet> readDataForPieChart(String customers, String year) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!StringUtils.isEmpty(customers) && customers.contains("[")) {
+            customers = customers.substring(1, customers.length() - 1);
+        }
+        List list = customerReportDao.read(customers, year);
+        List<Report2> reportList = new MappingEngine().getPieChartReport(list);
+        List<Report2DataSet> dataSets = new ArrayList<Report2DataSet>();
+        for (Report2 r : reportList) {
+            dataSets.add(new Report2DataSet(r.getTypeName(), r.getAmount()));
+        }
+        return dataSets;
     }
 
     public List<Report3DataSet> readDataForBarChart(String customers, String year) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!StringUtils.isEmpty(customers) && customers.contains("[")) {
+            customers = customers.substring(1, customers.length() - 1);
+        }
+        List list = customerReportDao.read(customers, year);
+        List<Report2> reportList = new MappingEngine().getPieChartReport(list);
+        List<Report3DataSet> dataSets = new ArrayList<Report3DataSet>();
+        for (Report2 r : reportList) {
+            dataSets.add(new Report3DataSet(r.getTypeName(), r.getAmount()));
+        }
+        return dataSets;
     }
 
     public List<Report4DataSet> readDataForColumnChart(String customers, String months, String year) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!StringUtils.isEmpty(customers) && customers.contains("[")) {
+            customers = customers.substring(1, customers.length() - 1);
+        }
+        String[] typeAr;
+        if (!StringUtils.isEmpty(customers) && customers.contains(",")) {
+            typeAr = customers.split("[,]");
+        } else {
+            typeAr = new String[]{customers};
+        }
+        if (!StringUtils.isEmpty(months) && months.contains("[")) {
+            months = months.substring(1, months.length() - 1);
+        }
+        String[] monthAr;
+        if (!StringUtils.isEmpty(months) && months.contains(",")) {
+            monthAr = months.split("[,]");
+        } else {
+            monthAr = new String[]{months};
+        }
+        int typeCount = typeAr.length;
+        List list = customerReportDao.read(customers, months, year);
+        List<Report1> reportList = new MappingEngine().getList(list);
+        logger.info(reportList.size());
+        List<Report4DataSet> dataSets = new ArrayList<Report4DataSet>();
+        for (int i = 0; i < typeCount; i++) {
+            List<DataPoint> dataPoints = constructDataPoints(reportList, typeAr[i].trim(), monthAr, i);
+            dataSets.add(new Report4DataSet("column", dataPoints, typeAr[i]));
+        }
+        return dataSets;
     }
 
     public List<Report5DataSet> readDataForLineChart(String customers, String months, String year) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!StringUtils.isEmpty(customers) && customers.contains("[")) {
+            customers = customers.substring(1, customers.length() - 1);
+        }
+        String[] typeAr;
+        if (!StringUtils.isEmpty(customers) && customers.contains(",")) {
+            typeAr = customers.split("[,]");
+        } else {
+            typeAr = new String[]{customers};
+        }
+        if (!StringUtils.isEmpty(months) && months.contains("[")) {
+            months = months.substring(1, months.length() - 1);
+        }
+        String[] monthAr;
+        if (!StringUtils.isEmpty(months) && months.contains(",")) {
+            monthAr = months.split("[,]");
+        } else {
+            monthAr = new String[]{months};
+        }
+        int typeCount = typeAr.length;
+        List list = customerReportDao.read(customers, months, year);
+        List<Report1> reportList = new MappingEngine().getList(list);
+        logger.info(reportList.size());
+        List<Report5DataSet> dataSets = new ArrayList<Report5DataSet>();
+        for (int i = 0; i < typeCount; i++) {
+            List<DataPoint> dataPoints = constructDataPoints(reportList, typeAr[i].trim(), monthAr, i);
+            dataSets.add(new Report5DataSet(dataPoints, typeAr[i]));
+        }
+        return dataSets;
     }
 
     public ReportDataSet getReports(String customers, String months, String year) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Report1DataSet> report1 = readDataForAreaChart(customers, months, year);
+        List<Report2DataSet> report2 = readDataForPieChart(customers, year);
+        List<Report3DataSet> report3 = readDataForBarChart(customers, year);
+        List<Report4DataSet> report4 = readDataForColumnChart(customers, months, year);
+        List<Report5DataSet> report5 = readDataForLineChart(customers, months, year);
+        ReportDataSet reportDataSet = new ReportDataSet();
+        reportDataSet.setReport1(report1);
+        reportDataSet.setReport2(report2);
+        reportDataSet.setReport3(report3);
+        reportDataSet.setReport4(report4);
+        reportDataSet.setReport5(report5);
+        return reportDataSet;
     }
     
     @ResponseBody
